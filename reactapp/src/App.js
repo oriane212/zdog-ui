d8e0355490b0b6ebaf747a323d6cf7dd7dac8900
Oriane <oriane.daley@gmail.com>
2021-01-28 23:19:10 -0500

    add Controls c, refactor app state

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,114 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './zdogui.css';
-import ShapeControls from './components/ShapeControls';
-import { FormControl, FormControlLabel, Input, InputLabel } from '@material-ui/core';
-import Checkbox from '@material-ui/core/Checkbox';
-import Slider from '@material-ui/core/Slider';
-import Typography from '@material-ui/core/Typography';
+import Controls from './components/Controls';
+
 
 function App(props) {
-  
-  const [canvas_w, setCanvas_w] = useState(240);
-  const [canvas_h, setCanvas_h] = useState(240);
-  const [dragRotate, setDragRotate] = useState(false);
-  const [animate, setAnimate] = useState(true);
-  const [rotate_x, setRotate_x] = useState(.06);
-  const [rotate_y, setRotate_y] = useState(0);
-  //const [shapes, setShapes] = useState([]);
 
-  /*
-  function updateShapes(e) {
-
-    // TODO
-
-    console.log(e.target.value);
+  const stateVars = {
+    canvas_w: useState(240),
+    canvas_h: useState(240),
+    dragRotate: useState(false),
+    animate: useState(true),
+    rotate_x: useState(.06),
+    rotate_y: useState(0),
+    shapes: useState([])
   }
-  */
 
-  /*
-  function addShape(newshapearry) {
-    this.setState({
-      shapes: newshapearry
-    })
-  }
-  */
+  return (
 
-    return (
+    <React.Fragment>
 
-      <main>
-        <section className="controls">
-          <section className="controls_illo">
+    <main>
+      
+      <Controls stateVars={stateVars}></Controls>
 
-            <div className="controlsContainer">
+      <section className="results">
 
-              <div className="parameter">
-                <FormControl>
-                  <InputLabel htmlFor="canvas_width">Canvas width</InputLabel>
-                  <Input id="canvas_width" value={canvas_w} disabled={false} onChange={(e) => setCanvas_w(e.target.value)} />
-                </FormControl>
-              </div>
-
-              <div className="parameter">
-                <FormControl>
-                  <InputLabel htmlFor="canvas_height">Canvas height</InputLabel>
-                  <Input id="canvas_height" value={canvas_h} disabled={false} onChange={(e,v) => setCanvas_h(e.target.value)} />
-                </FormControl>
-              </div>
-
-
-              <div className="parameter">
-                <FormControlLabel
-                  label="Drag Rotate"
-                  control={<Checkbox checked={dragRotate} onChange={() => setDragRotate(!dragRotate)} name="dragRotate" id="dragRotate" color="primary" />}
-                />
-              </div>
-
-
-              <div className="parameter">
-                <FormControlLabel
-                  label="Animate"
-                  control={<Checkbox checked={animate} onChange={() => setAnimate(!animate)} name="animate" id="animate" color="default" />}
-                />
-              </div>
-
-
-
-              <div className="sub-parameter">
-                <Typography id="rotate_x_label">Rotate x = {rotate_x}</Typography>
-                <Slider id="rotate_x" value={rotate_x} min={0} max={1} step={0.01} onChange={(e,v) => setRotate_x(v)} aria-labelledby="rotate_x_label" disabled={!animate} />
-              </div>
-
-              <div className="sub-parameter">
-                <Typography id="rotate_y_label">Rotate y = {rotate_y}</Typography>
-                <Slider id="rotate_y" value={rotate_y} min={0} max={1} step={0.01} onChange={(e,v) => setRotate_y(v)} aria-labelledby="rotate_y_label" disabled={!animate} />
-              </div>
-
-
-
-            </div>
-
-          </section>
-
-          
-          
-
+        <section className="illustration" width={stateVars.canvas_w[0]} height={stateVars.canvas_h[0]}>
+          <canvas id="illo" width={stateVars.canvas_w[0]} height={stateVars.canvas_h[0]}></canvas>
         </section>
+        
 
-        <section className="results">
+      </section>
 
-          <section className="illustration" width={canvas_w} height={canvas_h}>
-            <canvas id="illo" width={canvas_w} height={canvas_h}></canvas>
-          </section>
-          <section id="container"></section>
+    </main>
 
-        </section>
+  <section id="container"></section>
 
-      </main>
+  </React.Fragment>
 
-    );
-  
+  );
+
 }
 
 export default App;
-
-// <ShapeControls appstate={{values, shapes}} onAdd={this.addShape} onChange={this.updateShapes}></ShapeControls>
---
+++ reactapp/src/components/Controls.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import '../zdogui.css';
+import { FormControl, FormControlLabel, Input, InputLabel, makeStyles } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import Slider from '@material-ui/core/Slider';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    slider: {
+        width: 200
+    },
+    checkbox: {
+        'padding-bottom': 12
+    },
+    parameter: {
+        display: 'block',
+        margin: 16
+    },
+    subparameter: {
+        'margin-left': 32,
+        'margin-top': 3,
+        'margin-bottom': 3
+    }
+}));
+
+function Controls(props) {
+
+    const stateVars = props.stateVars;
+    const [canvas_w, setCanvas_w] = [stateVars.canvas_w[0], stateVars.canvas_w[1]];
+    const [canvas_h, setCanvas_h] = [stateVars.canvas_h[0], stateVars.canvas_h[1]];
+    const [dragRotate, setDragRotate] = [stateVars.dragRotate[0], stateVars.dragRotate[1]];
+    const [animate, setAnimate] = [stateVars.animate[0], stateVars.animate[1]];
+    const [rotate_x, setRotate_x] = [stateVars.rotate_x[0], stateVars.rotate_x[1]];
+    const [rotate_y, setRotate_y] = [stateVars.rotate_y[0], stateVars.rotate_y[1]];
+    const [shapes, setShapes] = [stateVars.shapes[0], stateVars.shapes[1]];
+
+    const classes = useStyles();
+
+    return (
+
+        <section className="controls">
+            <section className="controls_illo">
+                <div className="controlsContainer">
+
+                    <FormControl className={classes.parameter}>
+                        <InputLabel htmlFor="canvas_width">Canvas width</InputLabel>
+                        <Input id="canvas_width" value={canvas_w} disabled={false} onChange={(e) => setCanvas_w(e.target.value)} />
+                    </FormControl>
+
+                    <FormControl className={classes.parameter}>
+                        <InputLabel htmlFor="canvas_height">Canvas height</InputLabel>
+                        <Input id="canvas_height" value={canvas_h} disabled={false} onChange={(e, v) => setCanvas_h(e.target.value)} />
+                    </FormControl>
+
+                    <FormControl className={classes.parameter}>
+                        <FormControlLabel
+                            label="Drag Rotate"
+                            control={<Checkbox className={classes.checkbox} checked={dragRotate} onChange={() => setDragRotate(!dragRotate)} name="dragRotate" id="dragRotate" color="primary" />}
+                        />
+                    </FormControl>
+
+                    <FormControl className={classes.parameter}>
+                        <FormControlLabel
+                            label="Animate"
+                            control={<Checkbox className={classes.checkbox} checked={animate} onChange={() => setAnimate(!animate)} name="animate" id="animate" color="primary" />}
+                        />
+                    </FormControl>
+
+                    <FormControl className={classes.subparameter}>
+                        <Typography id="rotate_x_label">Rotate x = {rotate_x}</Typography>
+                        <Slider className={classes.slider} id="rotate_x" value={rotate_x} min={0} max={1} step={0.01} onChange={(e, v) => setRotate_x(v)} aria-labelledby="rotate_x_label" disabled={!animate} />
+                    </FormControl>
+
+                    <FormControl className={classes.subparameter}>
+                        <Typography id="rotate_y_label">Rotate y = {rotate_y}</Typography>
+                        <Slider className={classes.slider} id="rotate_y" value={rotate_y} min={0} max={1} step={0.01} onChange={(e, v) => setRotate_y(v)} aria-labelledby="rotate_y_label" disabled={!animate} />
+                    </FormControl>
+
+                </div>
+
+            </section>
+
+        </section>
+
+    )
+
+}
+
+export default Controls;
