35a2b27012f61cd653cb756e55ef5fe92da046da
Oriane <oriane.daley@gmail.com>
2021-07-08 12:31:12 -0400

    add ease animation script

--- reactapp/src/createScript.js
+++ reactapp/src/createScript.js
@@ -55,7 +55,10 @@ let ${name} = new Zdog['${shape.shapeClass}'](${dataString})
     let endString;
 
     if (illoVars.animate[0]) {
-        endString = `
+
+        // continuous spin
+        if (illoVars.animateSelection[0] === 'spin') {
+            endString = `
 function animate() {
     illo.rotate.x += ${illoVars.spin_x[0]};
     illo.rotate.y += ${illoVars.spin_y[0]};
@@ -63,9 +66,32 @@ function animate() {
     illo.updateRenderGraph();
     requestAnimationFrame(animate);
 }
+            
+    animate();
+        `
+        } else if (illoVars.animateSelection[0] === 'ease') {
+        endString = `
+/* Zdog animate fn for easeInOut */
+
+let ticker = 0;
+let cycleCount = ${illoVars.easeIO[0].cycleCount};
+
+function animate() {
+    let progress = ticker / cycleCount;
+    // apply easing to rotation
+    let tween = Zdog.easeInOut(progress % 1, ${illoVars.easeIO[0].power});
+    ${illoVars.easeIO[0].x ? 'illo.rotate.x = tween * Zdog.TAU;' : ''}
+    ${illoVars.easeIO[0].y ? 'illo.rotate.y = tween * Zdog.TAU;' : ''}
+    ${illoVars.easeIO[0].z ? 'illo.rotate.z = tween * Zdog.TAU;' : ''}
+    ticker++;
+
+    illo.updateRenderGraph();
+    requestAnimationFrame(animate);
+}
 
 animate();
         `
+        }
     } else if (!illoVars.animate[0] && illoVars.dragRotate[0]) {
         endString = `
 function animate() {
