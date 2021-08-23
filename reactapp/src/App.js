ec5de459ec9c46053e049f82bfa753a8ec3b1050
Oriane <oriane.daley@gmail.com>
2021-08-23 18:10:01 -0400

    add lego demo

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -47,77 +47,415 @@ const useStyles = makeStyles((theme) => ({
 
 let valuesarry = [200, 120, 40, 80, 160];
 
+
+let demos = {
+  none: {
+    fallback: "An animated illustration consisting of 3D shapes",
+    shapelayers: []
+  },
+  lego: {
+    fallback: 'Animated model of a red LEGO piece',
+    shapelayers: [
+      {
+        id: generateID(),
+        children: [
+          {
+            id: generateID(),
+            children: [
+              {
+                id: generateID(),
+                children: [],
+                open: true,
+                shapeClass: 'Cylinder',
+                data: {
+                  "color": "#c30006",
+                  "backface": "#aa0a11",
+                  "stroke": "1",
+                  "fill": true,
+                  "translate": {
+                    "x": -80,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "rotate": {
+                    "x": 0,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "diameter": "65",
+                  "length": "85",
+                  "frontFace": "#000000"
+                },
+                faces: {}
+              },
+              {
+                id: generateID(),
+                children: [],
+                open: true,
+                shapeClass: 'Cylinder',
+                data: {
+                  "color": "#c30006",
+                  "backface": "#aa0a11",
+                  "stroke": "1",
+                  "fill": true,
+                  "translate": {
+                    "x": 0,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "rotate": {
+                    "x": 0,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "diameter": "65",
+                  "length": "85",
+                  "frontFace": "#000000"
+                },
+                faces: {}
+              },
+              {
+                id: generateID(),
+                children: [],
+                open: true,
+                shapeClass: 'Cylinder',
+                data: {
+                  "color": "#c30006",
+                  "backface": "#aa0a11",
+                  "stroke": "1",
+                  "fill": true,
+                  "translate": {
+                    "x": 80,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "rotate": {
+                    "x": 0,
+                    "y": 0,
+                    "z": 0
+                  },
+                  "diameter": "65",
+                  "length": "85",
+                  "frontFace": "#000000"
+                },
+                faces: {}
+              }
+            ],
+            open: true,
+            shapeClass: 'Group',
+            data: {
+              "translate": {
+                "x": 0,
+                "y": 5,
+                "z": 0
+              },
+              "rotate": {
+                "x": 1.5707963267948966,
+                "y": 0,
+                "z": 0
+              }
+            },
+            faces: {}
+          }
+        ],
+        open: true,
+        shapeClass: 'Box',
+        data: {
+          /*         "addTo": illo, */
+          "color": "#c30005",
+          "backface": true,
+          "stroke": 1,
+          "fill": true,
+          "translate": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+          },
+          "rotate": {
+            "x": 0,
+            "y": 0,
+            "z": 0
+          },
+          "width": "320",
+          "height": "96",
+          "depth": "160",
+          "frontFace": "#b2081f",
+          "rearFace": "#c30005",
+          "topFace": "#db0623",
+          "bottomFace": false,
+          "leftFace": "#c30005",
+          "rightFace": "#c30005"
+        },
+        faces: {
+          "frontFace": true,
+          "rearFace": true,
+          "topFace": true,
+          "bottomFace": "#a20106",
+          "leftFace": true,
+          "rightFace": true
+        }
+      },
+      {
+        id: generateID(),
+        children: [
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#c30006",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": 40,
+                "y": 40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#c30006",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": 40,
+                "y": -40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#c30006",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": 120,
+                "y": 40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#c30006",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": 120,
+                "y": -40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#c30006",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": -120,
+                "y": 40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#b2081f",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": -120,
+                "y": -40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#b2081f",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": -40,
+                "y": 40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          },
+          {
+            id: generateID(),
+            children: [],
+            open: true,
+            shapeClass: 'Cylinder',
+            data: {
+              "color": "#b2081f",
+              "backface": true,
+              "stroke": 1,
+              "fill": true,
+              "translate": {
+                "x": -40,
+                "y": -40,
+                "z": 0
+              },
+              "rotate": {
+                "x": 0,
+                "y": 0,
+                "z": 0
+              },
+              "diameter": "50",
+              "length": "20",
+              "frontFace": "#db0723"
+            },
+            faces: {}
+          }
+        ],
+        open: true,
+        shapeClass: 'Group',
+        data: {
+          "translate": {
+            "x": 0,
+            "y": -58,
+            "z": 0
+          },
+          "rotate": {
+            "x": 1.5707963267948966,
+            "y": 0,
+            "z": 0
+          },
+          /*  "addTo": illo */
+        },
+        faces: {}
+      }
+    ]
+  }
+}
+
 function App(props) {
 
   const stateVars = {
-    canvas_w: useState(240),
-    canvas_h: useState(240),
+    canvas_w: useState(500),
+    canvas_h: useState(500),
     dragRotate: useState(true),
     animate: useState(false),
-    rotate_x: useState(5.7),
-    rotate_y: useState(.78),
-    rotate_z: useState(0),
+    rotate_x: useState(0),
+    rotate_y: useState(3.141592653589793),
+    rotate_z: useState(0.4363323129985824),
     spin_x: useState(.01),
     spin_y: useState(.04),
     spin_z: useState(.01),
     easeIO: useState(
       {
-        x: false,
+        x: true,
         y: true,
         z: false,
-        cycleCount: 150,
+        cycleCount: 400,
         power: 3
       }
     ),
-    animateSelection: useState('spin'),
-    fallback: useState("An animated illustration consisting of 3D shapes")
+    animateSelection: useState('ease'),
+    demo: useState('lego'),
+    fallback: useState(demos.lego.fallback)
   }
 
-  const addedShapes = useState([
-    {
-      id: generateID(),
-      children: [
-        {
-          id: generateID(),
-          children: [
-            {
-              id: generateID(),
-              children: [],
-              open: true,
-              shapeClass: 'Ellipse',
-              data: getDefaultValsForShapeProperties(zdogDefaultShapes, 'Ellipse').data,
-              faces: {}
-            }
-          ],
-          open: true,
-          shapeClass: 'Rect',
-          data: getDefaultValsForShapeProperties(zdogDefaultShapes, 'Rect').data,
-          faces: {}
-        }
-      ],
-      open: true,
-      shapeClass: 'Ellipse',
-      data: getDefaultValsForShapeProperties(zdogDefaultShapes, 'Ellipse').data,
-      faces: {}
-    },
-    {
-      id: generateID(),
-      children: [
-        {
-          id: generateID(),
-          children: [],
-          open: true,
-          shapeClass: 'Rect',
-          data: getDefaultValsForShapeProperties(zdogDefaultShapes, 'Rect').data,
-          faces: {}
-        }
-      ],
-      open: true,
-      shapeClass: 'Ellipse',
-      data: getDefaultValsForShapeProperties(zdogDefaultShapes, 'Ellipse').data,
-      faces: {}
-    }
-  ]);
+  const addedShapes = useState(demos.lego.shapelayers);
 
   const [open, setOpen] = useState(false);
 
@@ -153,17 +491,17 @@ function App(props) {
        data[prop] = defaultVal;
      }) */
 
-      props_basic.forEach((prop) => {
-        if (prop === 'translate' || prop === 'rotate') {
-          data[prop] = new Zdog.Vector({});
-          /* data[prop]['x'] = zdogShape[prop]['x'];
-          data[prop].y = zdogShape[prop].y;
-          data[prop].z = zdogShape[prop].z; */
-        } else {
-          let defaultVal = zdogShape[prop];
-          data[prop] = defaultVal;
-        }
-      })
+    props_basic.forEach((prop) => {
+      if (prop === 'translate' || prop === 'rotate') {
+        data[prop] = new Zdog.Vector({});
+        /* data[prop]['x'] = zdogShape[prop]['x'];
+        data[prop].y = zdogShape[prop].y;
+        data[prop].z = zdogShape[prop].z; */
+      } else {
+        let defaultVal = zdogShape[prop];
+        data[prop] = defaultVal;
+      }
+    })
 
     props_shape.forEach((prop) => {
       if (prop === 'width' || prop === 'height' || prop === 'depth' || prop === 'diameter' || prop === 'length' || prop === 'radius') {
@@ -177,7 +515,7 @@ function App(props) {
       }
     })
 
-    return {data, faces};
+    return { data, faces };
 
   }
 
@@ -202,7 +540,7 @@ function App(props) {
       flattened.push(newshape);
 
       // set selectedNodeId to newly added shape layer in tree
-      selectedNodeId[1]((flattened.length-1).toString());
+      selectedNodeId[1]((flattened.length - 1).toString());
 
     } else {
       let posStrings = nodeId.split('_');
@@ -222,7 +560,7 @@ function App(props) {
 
       // set selectedNodeId to newly added shape layer in tree
       let childcount = currentShape.children.length;
-      selectedNodeId[1](nodeId + '_' + (childcount-1));
+      selectedNodeId[1](nodeId + '_' + (childcount - 1));
     }
 
     addedShapes[1](flattened);
@@ -254,8 +592,8 @@ function App(props) {
         let divHTML = document.getElementById('editorHTML');
         let jarHTML = CodeJar(divHTML, Prism.highlightElement);
 
-        let snippet = 
-        `<canvas id="illo" width="${stateVars.canvas_w[0]}" height="${stateVars.canvas_h[0]}">
+        let snippet =
+          `<canvas id="illo" width="${stateVars.canvas_w[0]}" height="${stateVars.canvas_h[0]}">
           <p>${stateVars.fallback[0]}</p>
         </canvas>`;
 
@@ -308,7 +646,7 @@ function App(props) {
         <Toolbar>
           <Typography variant="h6">
             Zdog UI
-            </Typography>
+          </Typography>
           <Button onClick={getCode} color="inherit" startIcon={<CodeIcon />} aria-label="get code" className={classes.getCode}>Get Code</Button>
           <Dialog maxWidth="md" onClose={handleClose} open={open}>
             <Container className={classes.container}>
