74bda11c873e2b3e488072807affe2265ec67d09
Oriane <oriane.daley@gmail.com>
2022-01-26 20:56:02 -0500

    fix treeview toggling, all items expanded to start

--- reactapp/src/components/IlloExplorer.js
+++ reactapp/src/components/IlloExplorer.js
@@ -21,7 +21,8 @@ const useStyles = makeStyles({
 
     },
     item: {
-        color: 'rgba(0, 0, 0, 0.87)'
+        color: 'rgba(0, 0, 0, 0.87)',
+/*         borderLeft: '1px dashed grey' */
     }
 });
 
@@ -43,6 +44,7 @@ export default function IlloExplorer(props) {
     };
 
     const handleSelect = (event, nodeIds) => {
+        event.preventDefault();
         selectedNodeId[1](nodeIds);
         console.log('nodeIds: ' + nodeIds);
     };
@@ -54,6 +56,8 @@ export default function IlloExplorer(props) {
         return newArrayFlattened;
     }
 
+    // check that the parent of the selected item is expanded (ie. selected item is not hidden from user, which happens when a new item is added)
+    // also make sure top level Canvas node is always expanded
     function checkParentExpanded() {
         let pos = selectedNodeId[0].split('_');
         if (pos.length > 1) {
@@ -74,13 +78,18 @@ export default function IlloExplorer(props) {
         }
     }
 
+
+    let startingParentIds = [];
+
     function createTree(childrenArray, parentnodeid) {
         let p = parentnodeid;
+        startingParentIds.push(p);
+
         console.log(childrenArray.length !== 0);
         if (childrenArray.length !== 0) {
             let treeitems = childrenArray.map((shape, i) => {
                 let pos = `${p}_${i}`;
-                let item = (<TreeItem className={classes.item} key={generateID()} nodeId={pos} label={fixCamelCase(shape.shapeClass)}>
+                let item = (<TreeItem onLabelClick={handleSelect} onIconClick={handleToggle} className={classes.item} key={generateID()} nodeId={pos} label={fixCamelCase(shape.shapeClass)}>
                     {createTree(shape.children, pos)}
                 </TreeItem>);
                 return item;
@@ -93,7 +102,7 @@ export default function IlloExplorer(props) {
 
     let toptreelevel = addedShapes[0].map((shape, i) => {
         let item =
-            (<TreeItem className={classes.item} key={generateID()} nodeId={i.toString()} label={fixCamelCase(shape.shapeClass)}>
+            (<TreeItem onLabelClick={handleSelect} onIconClick={handleToggle} className={classes.item} key={generateID()} nodeId={i.toString()} label={fixCamelCase(shape.shapeClass)}>
                 {createTree(shape.children, i.toString())}
             </TreeItem>);
         return item;
@@ -102,6 +111,14 @@ export default function IlloExplorer(props) {
     useEffect(() => {
         checkParentExpanded();
     });
+
+    useEffect(() => {
+        // check that all items are expanded to start
+        let copied = copyArray(expanded);
+        let newArry = copied.concat(startingParentIds);
+        setExpanded(newArry);
+        console.log(newArry);
+    }, []);
 
 
     return (
@@ -113,12 +130,12 @@ export default function IlloExplorer(props) {
                 className={classes.root}
                 defaultCollapseIcon={<ExpandMoreIcon />}
                 defaultExpandIcon={<ChevronRightIcon />}
-                expanded={expanded}
+                expanded ={expanded}
                 selected={selectedNodeId[0]}
                 onNodeToggle={handleToggle}
                 onNodeSelect={handleSelect}
             >
-                <TreeItem className={classes.item} nodeId="canvasnode" label="Canvas">
+                <TreeItem onLabelClick={handleSelect} onIconClick={handleToggle} className={classes.item} nodeId="canvasnode" label="Canvas">
                     {toptreelevel}
                 </TreeItem>
             </TreeView>
